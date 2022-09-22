14354cdd5f26a456a9933970f93ed9803f0d4eb6
daviddiazh <daviddiazlds1221@gmail.com>
2022-09-22 00:01:32 -0500

    Footer responsive OK

--- components/Ui/public/MenuFooter.jsx
+++ components/Ui/public/MenuFooter.jsx
@@ -55,6 +55,38 @@ export const MenuFooter = ({ t }) => {
           Copyrights Soy Tú Salud © {currentYear}. {t('footer:copyrights')}
         </p>
       </footer>
+
+      <footer className="footer-mobile">
+        <div className="container-footer">
+          <div className="img-footer-mobile">
+            <Image
+              src="/logo_vertical-white.png"
+              width="150"
+              height="150"
+              alt="logo"
+            />
+          </div>
+          <div>
+            <div className="contact-info-footer">
+              <h4 className="footer-title">Contáctanos</h4>
+              <p>Medellín</p>
+              <p>Teléfono +57 320 702 3823</p>
+              <p>Email: </p>
+              <p>info@fundacionsoytusalud.org</p>
+            </div>
+            <div>
+              <h4 className="footer-title">TeAcompaño</h4>
+              <p className="description-footer">
+                Ayúdanos a cambiar la vida de los menos favorecidos en Colombia.
+              </p>
+              <button className="btn-footer">TE ACOMPAÑO</button>
+            </div>
+          </div>
+        </div>
+        <p className="footer-copyright">
+          Copyrights Soy Tú Salud © {currentYear}. {t('footer:copyrights')}
+        </p>
+      </footer>
     </>
   )
 }
