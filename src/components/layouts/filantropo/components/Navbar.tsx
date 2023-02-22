58f11435e0d88b272203ee9a782af073fa160214
Interlude <juan.cabrerar@sofka.com.co>
2023-02-21 20:06:56 -0500

    index

--- src/components/layouts/filantropo/components/Navbar.tsx
+++ src/components/layouts/filantropo/components/Navbar.tsx
@@ -1,12 +1,12 @@
-import {Container} from "@mui/material";
+import { Container } from "@mui/material";
 import Image from "next/image";
-import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
-import ArrowDropDownOutlinedIcon from '@mui/icons-material/ArrowDropDownOutlined';
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import ArrowDropDownOutlinedIcon from "@mui/icons-material/ArrowDropDownOutlined";
 
 const Navbar = () => {
   return (
-    <div style={{backgroundColor: "#616AC5"}}>
-      <div className="relative container  w-full " >
+    <div style={{ backgroundColor: "#616AC5" }} className="sticky flex top-0 left-0 z-20">
+      <div className="relative container  w-full ">
         <div className="w-full  flex py-3 max-w-screen-2xl justify-between items-center">
           <div className="flex items-center space-x-10  ">
             <Image
@@ -16,14 +16,21 @@ const Navbar = () => {
               alt="logo"
               quality={100}
             />
-            <span className="text-white back px-8 rounded-xl font-semibold " style={{backgroundColor: "#E26D83"}} >Filántropo</span>
+            <span
+              className="text-white back px-8 rounded-xl font-semibold "
+              style={{ backgroundColor: "#E26D83" }}
+            >
+              Filántropo
+            </span>
           </div>
           <div className="flex space-x-9">
             <ul>
               <li className="text-white font-semibold ">Inicio</li>
             </ul>
             <ul>
-              <li className="text-white font-semibold">Proyectos de Maternidad</li>
+              <li className="text-white font-semibold">
+                Proyectos de Maternidad
+              </li>
             </ul>
             <ul>
               <li className="text-white  font-semibold">Apoyos</li>
@@ -36,12 +43,10 @@ const Navbar = () => {
               </li>
             </ul>
           </div>
-
         </div>
       </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default Navbar
+export default Navbar;
--- src/pages/filantropo/index.tsx
+++ src/pages/filantropo/index.tsx
@@ -1,54 +1,228 @@
-import {ReactElement} from "react";
-import {LayoutFilantropo} from "@/src/components/layouts/filantropo/LayoutFilantropo";
-import heart from '@/public/heart2.png'
+import { ReactElement } from "react";
+import { LayoutFilantropo } from "@/src/components/layouts/filantropo/LayoutFilantropo";
+import heart from "@/public/heart2.png";
 import Image from "next/image";
-
-
+import RestoreIcon from "@mui/icons-material/Restore";
+import FemaleIcon from "@mui/icons-material/Female";
+import SimCardDownloadOutlinedIcon from "@mui/icons-material/SimCardDownloadOutlined";
+import pregnantWoman from "@/public/PregnantWoman.jpg";
 
 const Filantropo = () => {
   return (
-    <div style={{backgroundColor: "#FEFEFE"}}>
-      <div className="container">
-        <div style={{backgroundColor:"#EFF2FB"}} className="rounded-xl flex flex-col">
-          <div className="absolute self-end -z-0" style={{marginRight:"5%"}}>
-            <Image src={heart} alt="heart"  />
+    <div style={{ backgroundColor: "#FEFEFE" }}>
+      <div className="container space-y-9">
+        <div
+          style={{ backgroundColor: "#EFF2FB" }}
+          className="rounded-xl flex flex-col"
+        >
+          <div className="absolute self-end -z-0" style={{ marginRight: "5%" }}>
+            <Image src={heart} alt="heart" />
           </div>
           <div className="space-y-5 px-5 py-8 z-10">
-            <h1 style={{color:"#E26D83"}} className="font-bold text-4xl" >Bienvenido, usuario</h1>
-            <h2 style={{color:"#E26D83"}} className="font-extrabold text-xl" >Resumen de mi cuenta</h2>
-            <div className=" items-center flex space-x-5 ">
-              <ul style={{backgroundColor: "#616AC5"}} className="rounded-xl py-5 px-4 w-1/4 flex items-center space-x-3" >
+            <h1 style={{ color: "#E26D83" }} className="font-bold text-4xl">
+              Bienvenido, usuario
+            </h1>
+            <h2 style={{ color: "#E26D83" }} className="font-extrabold text-xl">
+              Resumen de mi cuenta
+            </h2>
+            <div
+              className=" items-center flex space-x-5 "
+              style={{ display: "-webkit-box", paddingRight: "61px" }}
+            >
+              <ul
+                style={{ backgroundColor: "#616AC5" }}
+                className="rounded-xl py-5 px-4 w-1/4 flex items-center space-x-3"
+              >
                 <li className="text-white font-bold text-4xl">3</li>
                 <li className="text-white font-semibold">Madres que apoyo</li>
               </ul>
-              <ul style={{backgroundColor: "#616AC5"}} className="rounded-xl py-5 px-4 w-1/4 flex items-center space-x-3" >
+              <ul
+                style={{ backgroundColor: "#616AC5" }}
+                className="rounded-xl py-5 px-4 w-1/4 flex items-center space-x-3"
+              >
                 <li className="text-white font-bold text-4xl">5</li>
                 <li className="text-white font-semibold">Tratamientos</li>
               </ul>
-              <ul style={{backgroundColor: "#616AC5"}} className="rounded-xl py-5 px-4 w-1/4 flex items-center space-x-3" >
+              <ul
+                style={{ backgroundColor: "#616AC5" }}
+                className="rounded-xl py-5 px-4 w-1/4 flex items-center space-x-3"
+              >
                 <li className="text-white font-bold text-4xl">2</li>
-                <li className="text-white font-semibold">Reportes nuevos de salud</li>
+                <li className="text-white font-semibold">
+                  Reportes nuevos de salud
+                </li>
               </ul>
-              <ul style={{backgroundColor: "#616AC5"}} className="rounded-xl py-5 px-4 w-1/4 flex items-center space-x-3" >
+              <ul
+                style={{ backgroundColor: "#616AC5" }}
+                className="rounded-xl py-5 px-4 w-1/4 flex items-center space-x-3"
+              >
                 <li className="text-white font-bold text-4xl">3</li>
                 <li className="text-white font-semibold">Madres que apoyo</li>
               </ul>
             </div>
           </div>
         </div>
-
+        <div className="space-y-5 px-5 text-sm ">
+          <h1 className="text-black font-semibold text-2xl ">
+            {" "}
+            ¿Qué deseas hacer hoy?
+          </h1>
+          <div
+            className=" items-center flex space-x-5 "
+            style={{ display: "-webkit-box", paddingRight: "61px" }}
+          >
+            <ul
+              style={{ backgroundColor: "#616AC5" }}
+              className="rounded-xl py-5 px-3 w-1/4 flex items-center"
+            >
+              <li className="text-white font-bold text-4xl self-center">
+                <FemaleIcon sx={{ fontSize: "50px" }} />
+              </li>
+              <li className="text-white font-semibold">
+                Ver madres que necesitan apoyo
+              </li>
+            </ul>
+            <ul
+              style={{ backgroundColor: "#616AC5" }}
+              className="rounded-xl py-5 px-3 w-1/4 flex items-center space-x-2"
+            >
+              <li className="text-white font-bold text-4xl">
+                <RestoreIcon sx={{ fontSize: "50px" }} />
+              </li>
+              <li className="text-white font-semibold">
+                Historial de apoyos de la cuenta
+              </li>
+            </ul>
+            <ul
+              style={{ backgroundColor: "#616AC5" }}
+              className="rounded-xl py-5 px-3 w-1/4 flex items-center space-x-2"
+            >
+              <li className="text-white font-bold text-4xl">
+                <SimCardDownloadOutlinedIcon sx={{ fontSize: "50px" }} />
+              </li>
+              <li className="text-white font-semibold">
+                Descargar certificado de donación
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="space-y-5 px-5 text-sm ">
+          <h1 className="text-black font-semibold text-2xl ">
+            Madres que apoyo
+          </h1>
+          <div
+            className=" items-center flex space-x-5 "
+            style={{ display: "-webkit-box", paddingRight: "61px" }}
+          >
+            <ul
+              style={{ backgroundColor: "#EFF2FB" }}
+              className="rounded-xl py-5 px-3 w-1/4 flex items-center justify-center"
+            >
+              <div className="text-black font-semibold flex flex-col space-y-3">
+                <div className="h-32 w-32 relative">
+                  <Image
+                    src={pregnantWoman}
+                    alt="pregnantWoman"
+                    layout="fill"
+                    objectFit="cover"
+                    className="rounded-full"
+                  />
+                </div>
+                <div className="flex flex-col">
+                  <h1>Paula Andrea Rojas</h1>
+                  <span className="self-center">21 años</span>
+                </div>
+                <div  className="flex flex-col" >
+                  <li style={{ backgroundColor: "#616AC5" }} className="text-white font-semibold rounded-full self-center px-5 py-2">Ver mis apoyos</li>
+                </div>
+               
+              </div>
+            </ul>
+            <ul
+              style={{ backgroundColor: "#EFF2FB" }}
+              className="rounded-xl py-5 px-3 w-1/4 flex items-center justify-center"
+            >
+              <div className="text-black font-semibold flex flex-col space-y-3">
+                <div className="h-32 w-32 relative">
+                  <Image
+                    src={pregnantWoman}
+                    alt="pregnantWoman"
+                    layout="fill"
+                    objectFit="cover"
+                    className="rounded-full"
+                  />
+                </div>
+                <div className="flex flex-col">
+                  <h1>Paula Andrea Rojas</h1>
+                  <span className="self-center">21 años</span>
+                </div>
+                <div  className="flex flex-col" >
+                  <li style={{ backgroundColor: "#616AC5" }} className="text-white font-semibold rounded-full self-center px-5 py-2">Ver mis apoyos</li>
+                </div>
+               
+              </div>
+            </ul>
+            <ul
+              style={{ backgroundColor: "#EFF2FB" }}
+              className="rounded-xl py-5 px-3 w-1/4 flex items-center justify-center"
+            >
+              <div className="text-black font-semibold flex flex-col space-y-3">
+                <div className="h-32 w-32 relative">
+                  <Image
+                    src={pregnantWoman}
+                    alt="pregnantWoman"
+                    layout="fill"
+                    objectFit="cover"
+                    className="rounded-full"
+                  />
+                </div>
+                <div className="flex flex-col">
+                  <h1>Paula Andrea Rojas</h1>
+                  <span className="self-center">21 años</span>
+                </div>
+                <div  className="flex flex-col" >
+                  <li style={{ backgroundColor: "#616AC5" }} className="text-white font-semibold rounded-full self-center px-5 py-2">Ver mis apoyos</li>
+                </div>
+               
+              </div>
+            </ul>
+            <ul
+              style={{ backgroundColor: "#EFF2FB" }}
+              className="rounded-xl py-5 px-3 w-1/4 flex items-center justify-center"
+            >
+              <div className="text-black font-semibold flex flex-col space-y-3">
+                <div className="h-32 w-32 relative">
+                  <Image
+                    src={pregnantWoman}
+                    alt="pregnantWoman"
+                    layout="fill"
+                    objectFit="cover"
+                    className="rounded-full"
+                  />
+                </div>
+                <div className="flex flex-col">
+                  <h1>Paula Andrea Rojas</h1>
+                  <span className="self-center">21 años</span>
+                </div>
+                <div  className="flex flex-col" >
+                  <li style={{ backgroundColor: "#616AC5" }} className="text-white font-semibold rounded-full self-center px-5 py-2">Ver mis apoyos</li>
+                </div>
+               
+              </div>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-Filantropo.getLayout = function getLayout(page:ReactElement , props: any) {
-
+Filantropo.getLayout = function getLayout(page: ReactElement, props: any) {
   return (
-    <LayoutFilantropo  _nextI18Next = {props._nextI18Next}>
+    <LayoutFilantropo _nextI18Next={props._nextI18Next}>
       {page}
     </LayoutFilantropo>
-  )
-}
+  );
+};
 
-export default Filantropo
+export default Filantropo;
